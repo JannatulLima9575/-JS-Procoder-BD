5cc05a14ef855420b1922fea37198c7d9cfafe8c
Jannatul <limajannat1675@gmail.com>
2025-02-25 03:06:41 +0600

    added index.html

--- DOM-1/scripts.js
+++ DOM-1/scripts.js
@@ -8,3 +8,23 @@ const project = document.querySelector('.project'); //select using class
 project.style.color = 'green';
 project.style.fontSize = '20px';
 project.style.fontSize = 'bold';
+
+// forEach loop in querySelector
+const menuItem = document.querySelectorAll('a');
+// menuItem[1].style.color = 'red';
+menuItem.forEach(function(i){
+    i.style.color = 'black'
+})
+
+
+
+// using getElementBy ID/Class/Tag
+const useId = document.getElementById('box');
+const useClass = document.getElementsByClassName('box');
+
+const useTag = document.getElementsByTagName('p'); // you can't access directly in tag, you should to use loop like forEach loop and for loop etc.
+const convertArray = Array.from(useTag); // tag convert to array
+convertArray.forEach(tag => {
+    tag.style.color = 'blue';
+    tag.style.fontSize = '20px';
+});
