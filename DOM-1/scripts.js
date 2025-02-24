9d0d5118aacf063171fc62d0396713979965b197
Jannatul <limajannat1675@gmail.com>
2025-02-25 02:23:26 +0600

    added scripts.js file

---
+++ DOM-1/scripts.js
@@ -0,0 +1,10 @@
+// using of querySelector methods
+document.querySelector('h1').innerText = 'Jannatul Lima'; // for select h1 tag
+
+const body = document.querySelector('#head'); // select using ID
+body.style.margin = "20%";
+
+const project = document.querySelector('.project'); //select using class 
+project.style.color = 'green';
+project.style.fontSize = '20px';
+project.style.fontSize = 'bold';
