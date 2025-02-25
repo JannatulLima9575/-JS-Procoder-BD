894f89df6bc2f061d3e63b6db7d46dae95eae7b9
Jannatul <limajannat1675@gmail.com>
2025-02-25 15:44:25 +0600

    added scripts.js

--- DOM-2/scripts.js
+++ DOM-2/scripts.js
@@ -1,2 +1,16 @@
+// Add text inside tags
 let headTitle = document.querySelector('h1');
 headTitle.innerText = headTitle.innerText + ' Jannatul Lima';
+
+// Change text 
+let boxes = document.querySelectorAll('.box');
+boxes[0].innerText = 'HTML';
+boxes[1].innerText = 'Css';
+boxes[2].innerText = 'JavaScript';
+
+// using for loop if there will be a lots of content
+/*let newNum = 1;
+for(box of boxes) {
+    box.innerText = `Box No ${newNum}`;
+    newNum++;
+}*/
