40ecc101b296f5de52121e9a0bd5db05f3a4e749
Jannatul <limajannat1675@gmail.com>
2025-02-27 15:24:07 +0600

    added js functionalities

---
+++ Math/scripts.js
@@ -0,0 +1,36 @@
+// JavaScript Math 
+let num = 30.985432323;
+let numTow = 2;
+// if need Rounded Number 
+console.log(Math.round(num));
+console.log(Math.ceil(num)); // if want increase one number in decimal case
+console.log(Math.floor(num)); // if want decrease one number in decimal case
+
+// if need square value
+console.log(Math.pow(4,3));
+
+// for Root value
+console.log(Math.sqrt(64));
+
+// if you get positive output through result is negative
+console.log(Math.abs(-5));
+
+// for get minimum and maximum value
+console.log(Math.min(1,2,3,4,5,6,7)); //for minimum
+console.log(Math.max(1,2,3,4,5,6,7)); // for maximum
+
+// for pie value
+console.log(Math.PI);
+
+// for Trigonometry
+console.log(Math.sin((90 * Math.PI) / 180));
+console.log(Math.sin((0 * Math.PI) / 180));
+console.log(Math.cos((90 * Math.PI) / 180));
+console.log(Math.cos((0 * Math.PI) / 180));
+
+// if you get random number 
+console.log(Math.random());
+console.log(Math.random().toFixed(2)); // get two number after decimal (.)
+
+
+// Make a ludo
